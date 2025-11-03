079b11a4325b5abd4376a7cc500c1c6e011dec74
ryanftseng <rtseng99@yahoo.com>
2025-11-03 09:09:11 -0800

    mouse search

--- ottsim.py
+++ ottsim.py
@@ -1,7 +1,4 @@
 import random
-import numpy as np
-import matplotlib.pyplot as plt
-from collections import defaultdict
 import pygame
 
 #Grid Parameters
@@ -37,9 +34,6 @@ class Tile:
         self.organism = None
     def is_empty(self):
         return self.organism == None
-    
-
-
 
 #Otter class
 class Otter:
@@ -58,11 +52,10 @@ class Otter:
         self.endurance = random.randrange(1,100)
         self.hunger = 100
 
-    def move(self, width, depth):
+    def move(self, width, depth, grid):
         dx, dy = random.choice([(1,0), (-1,0), (0,1), (0,-1), (0,0)])
         self.x = max(0, min(width-1, self.x + dx))
         self.y = max(0, min(depth-1, self.y + dy))
-
 
 #Prey class
 class Prey:
@@ -73,20 +66,21 @@ class Prey:
 otter_list = []
 prey_list = [] 
 
+SIM_SPEED = 1000
+OTTER_TIMING = 5000
+URCHIN_TIMING = 3000
 
 #otter spawn event
 OTTER_SPAWN = pygame.USEREVENT+1
-pygame.time.set_timer(OTTER_SPAWN, 5000)
-
+pygame.time.set_timer(OTTER_SPAWN, OTTER_TIMING)
 
 #urchin spawn event
 URCHIN_SPAWN = pygame.USEREVENT+2
-pygame.time.set_timer(URCHIN_SPAWN, 3000)
-
+pygame.time.set_timer(URCHIN_SPAWN, URCHIN_TIMING)
 
 #Map update event
 UPDATE = pygame.USEREVENT+3
-pygame.time.set_timer(UPDATE, 500)
+pygame.time.set_timer(UPDATE, SIM_SPEED)
 
 #lifespan tick function
 def decrease_lifespan(organism_list):
@@ -97,12 +91,9 @@ def decrease_lifespan(organism_list):
             temp_list.append(o)
     return temp_list
     
-
 #Initialize Grid
 grid = []
 grid = [[Tile(x, y, "water") for y in range(DEPTH)] for x in range(WIDTH)]
-
-
 
 clock = pygame.time.Clock()
 running = True
@@ -115,10 +106,10 @@ while running:
         if event.type == UPDATE:
             #update movements and hunger
             for o in otter_list:
-                o.hunger-= (1 + 1/o.endurance)
+                o.hunger-= (3 + 3/o.endurance)
                 if o.hunger<= 0:
                     o.lifespan = 0
-                o.move(WIDTH, DEPTH)
+                #o.move(WIDTH, DEPTH, grid)
 
             #update lifespans
             otter_list = decrease_lifespan(otter_list)
@@ -129,15 +120,27 @@ while running:
             spawn_x = random.randrange(WIDTH)
             spawn_y = random.randrange(5)
             if grid[spawn_x][spawn_y].organism == None:
-                otter_list.append(Otter(spawn_x,random.randrange(DEPTH), 100))
-            grid[spawn_x][spawn_y].organism = "otter"
+                new_otter = Otter(spawn_x, spawn_y, 100.0)
+                otter_list.append(new_otter)
+                grid[spawn_x][spawn_y].organism = new_otter
         #spawn urchin
         if event.type == URCHIN_SPAWN:
             print("spawning urchin")
             spawn_x = random.randrange(WIDTH)
             if not any(p.x == spawn_x for p in prey_list):
                 prey_list.append(Prey(spawn_x, DEPTH, 100))
-
+        #mouse clicks
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = event.pos
+            grid_x = mx // CELL_SIZE
+            grid_y = my // CELL_SIZE
+            print(grid_x, grid_y)
+            if grid_y < DEPTH:
+                tile = grid[grid_x][grid_y]
+                if not tile.is_empty():
+                    print("Selected otter:", tile.organism.hunger)
+                else:
+                    print("empty tile")
 
     # Draw
     screen.fill((0, 0, 0))
@@ -147,7 +150,6 @@ while running:
             color = (32, 12, 255)
             pygame.draw.rect(screen, color, (x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE), width = 1)
 
-
     #draw otters
     for o in otter_list:
         screen.blit(o.sprite, (o.x*CELL_SIZE, o.y*CELL_SIZE))
@@ -156,6 +158,4 @@ while running:
     for p in prey_list:
         pygame.draw.circle(screen, (255, 0, 255), (p.x*CELL_SIZE+CELL_SIZE//2, (p.y*CELL_SIZE+CELL_SIZE//2)-50), 10)
 
-    
-    
     pygame.display.flip()

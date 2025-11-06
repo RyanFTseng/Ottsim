73d84eaa8381256958a7c262ee638ba9e3cd081c
ryanftseng <rtseng99@yahoo.com>
2025-11-06 10:06:15 -0800

    state logic

--- ottsim.py
+++ ottsim.py
@@ -15,8 +15,40 @@ WINDOW_SIZE = (WIDTH*CELL_SIZE + 1, DEPTH*CELL_SIZE + 175)
 GRID_TOGGLE = True
 PAUSE_TOGGLE = False
 
+
+#Timing controls
+EATING_TIME = 20
+
+SIM_SPEED = 3000
+OTTER_TIMING = 10000
+URCHIN_TIMING = 5000
+
+sim_accumulator = 0
+otter_accumulator = 0
+urchin_accumulator = 0
+
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
+
+#Grid tile class
+class Tile:
+    def __init__(self, x, y, terrain):
+        self.x = x
+        self.y = y
+        self.terrain = terrain
+        self.organism = None
+    def add_organism(self, organism):
+        self.organism = organism
+    def remove_organism(self):
+        self.organism = None
+    def is_empty(self):
+        return self.organism == None
+
+#Initialize Grid
+grid = []
+grid = [[Tile(x, y, "water") for y in range(DEPTH)] for x in range(WIDTH)]
+for i in range(WIDTH):
+    grid[i][DEPTH-1].terrain = "stone"
 
 
 #load sprites
@@ -34,20 +66,6 @@ def tint_image(image, tint_color):
     tinted.blit(tint_surface, (0, 0), special_flags=pygame.BLEND_RGBA_ADD)
     return tinted
 
-#Grid tile class
-class Tile:
-    def __init__(self, x, y, terrain):
-        self.x = x
-        self.y = y
-        self.terrain = terrain
-        self.organism = None
-    def add_organism(self, organism):
-        self.organism = organism
-    def remove_organism(self):
-        self.organism = None
-    def is_empty(self):
-        return self.organism == None
-
 #Otter class
 class Otter:
     def __init__(self, x, y, life_span):
@@ -56,20 +74,27 @@ class Otter:
         self.tint = (random.randrange(255), random.randrange(255), random.randrange(255))
         self.sprite = tint_image(otter_sprite, self.tint)
         #number of updates needed to finish eating (1-5)
+        self.eating_time = EATING_TIME
         self.damage = random.randrange(1,5)
         #predator escape rate (0-75)
         self.luck  = random.randrange(75)
         #hunger depletion rate (1-100)
         self.endurance = random.randrange(1,100)
         self.hunger = 100
-        self.inventory = "empty"
+        self.inventory = "prey"
         self.rect = self.sprite.get_rect(topleft=(self.x * CELL_SIZE, self.y * CELL_SIZE))
+        self.state = "move"
 
     def move(self, width, depth, grid):
         #generate movement direction
         dx, dy = random.choice([(1,0), (-1,0), (0,1), (0,-1), (0,0)])
         newx = max(0, min(width-1, self.x + dx))
         newy = max(0, min(depth-1, self.y + dy))
+        #harvest if chosen tile has prey
+        if grid[newx][newy].organism.__class__ == Prey:
+            self.inventory = "prey"
+            grid[newx][newy].organism.lifespan = 0
+            grid[newx][newy].organism = None
         #check if chosen tile is empty
         if grid[newx][newy].organism == None:
             #clear current tile
@@ -80,6 +105,22 @@ class Otter:
             self.rect = self.sprite.get_rect(topleft=(newx * CELL_SIZE, newy * CELL_SIZE))
             #set new tile's organism to self
             grid[self.x][self.y].organism = self
+
+    def update(self):
+        #state logic
+        if(self.inventory == "prey" and self.y == 0):
+            self.state = "eating"
+        if(self.state == "eating"):
+            self.eating_time -= self.damage
+            if self.eating_time == 0:
+                self.state = "move"
+                self.inventory = "empty"
+                self.hunger +=20
+                self.eating_time == EATING_TIME
+        else:
+            self.move(WIDTH, DEPTH, grid)
+        
+
 
 #Prey class
 class Prey:
@@ -92,19 +133,8 @@ class Prey:
 otter_list = []
 prey_list = [] 
 
-SIM_SPEED = 3000
-OTTER_TIMING = 10000
-URCHIN_TIMING = 5000
 
-sim_accumulator = 0
-otter_accumulator = 0
-urchin_accumulator = 0
 
-#Initialize Grid
-grid = []
-grid = [[Tile(x, y, "water") for y in range(DEPTH)] for x in range(WIDTH)]
-for i in range(WIDTH):
-    grid[i][DEPTH-1].terrain = "stone"
 
 #GUI
 manager = pygame_gui.UIManager(WINDOW_SIZE)
@@ -137,7 +167,7 @@ def update_sim(otter_list, prey_list):
         o.hunger-= (3 + 3/o.endurance)
         if o.hunger<= 0:
             o.lifespan = 0
-        o.move(WIDTH, DEPTH, grid)
+        o.update()
 
     #update lifespans
     otter_list = decrease_lifespan(otter_list)
